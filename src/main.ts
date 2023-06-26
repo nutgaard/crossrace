4bb421e8b889b3a5daf3a6dc6fa73cfa6aa7f38f
Nicklas Utgaard <nutgaard@gmail.com>
2023-06-26 21:27:31 +0200

    inline script in zip distribution and delay app initialization

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,36 @@ import './style.css';
 import './crossrace.css';
 import {findOrdering, parseRacers} from "./crossrace";
 
-const input = document.getElementById('input') as HTMLTextAreaElement;
-const debug = document.getElementById('debug') as HTMLTextAreaElement;
-const output = document.getElementById('output') as HTMLTextAreaElement;
-const racersInput = document.getElementById('racers') as HTMLTextAreaElement;
-
-let racers = parseRacers(racersInput.value);
-let ordering = findOrdering(input.value);
-updateOutput();
-
-input.addEventListener('input', (e) => {
-    const target = e.target as HTMLTextAreaElement;
-    ordering = findOrdering(target.value);
-    updateOutput();
+window.addEventListener('DOMContentLoaded', () => {
+    startApp()
 });
 
-racersInput.addEventListener('input', (e) => {
-    const target = e.target as HTMLTextAreaElement;
-    racers = parseRacers(target.value);
-    console.log('racers', racers);
-    updateOutput();
-});
+function startApp() {
+    const input = document.getElementById('input') as HTMLTextAreaElement;
+    const debug = document.getElementById('debug') as HTMLTextAreaElement;
+    const output = document.getElementById('output') as HTMLTextAreaElement;
+    const racersInput = document.getElementById('racers') as HTMLTextAreaElement;
 
-function updateOutput() {
-    debug.value = JSON.stringify(ordering, null, 2);
-    output.value = ordering
-        .map(it => `${it.rider} ${racers[it.rider] ?? '???'}`).join('\n');
+    let racers = parseRacers(racersInput.value);
+    let ordering = findOrdering(input.value);
+    updateOutput();
+
+    input.addEventListener('input', (e) => {
+        const target = e.target as HTMLTextAreaElement;
+        ordering = findOrdering(target.value);
+        updateOutput();
+    });
+
+    racersInput.addEventListener('input', (e) => {
+        const target = e.target as HTMLTextAreaElement;
+        racers = parseRacers(target.value);
+        console.log('racers', racers);
+        updateOutput();
+    });
+
+    function updateOutput() {
+        debug.value = JSON.stringify(ordering, null, 2);
+        output.value = ordering
+            .map(it => `${it.rider} ${racers[it.rider] ?? '???'}`).join('\n');
+    }
 }
